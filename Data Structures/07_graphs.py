8a1af4d93ea0e23aac4f7c27f00c10db3dcdc11e
anshulpodaar <anshulpodaar@gmail.com>
2023-07-04 09:57:17 +0200

    graphs update

--- Data Structures/07_graphs.py
+++ Data Structures/07_graphs.py
@@ -7,8 +7,10 @@ class Graph:
         self.adj_list = {}
 
     def print_graph(self):
+        print()
         for vertex in self.adj_list:
             print(vertex, ":", self.adj_list[vertex])
+        print()
 
     def add_vertex(self, vertex):
         if vertex not in self.adj_list.keys():
