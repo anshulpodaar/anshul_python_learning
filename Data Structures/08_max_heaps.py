e3fff98264b882e79208998f6834b3679df437b1
anshulpodaar <anshulpodaar@gmail.com>
2023-08-02 01:12:17 +0200

    Data Structures - Max_Heaps and Min_Heaps

---
+++ Data Structures/08_max_heaps.py
@@ -0,0 +1,94 @@
+"""
+Max Heaps
+"""
+
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+
+    def _left_child(self, index):
+        return 2 * index + 1
+
+    def _right_child(self, index):
+        return 2 * index + 2
+
+    def _parent(self, index):
+        return (index - 1) // 2
+
+    def _swap(self, index1, index2):
+        self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
+
+    def print_heap(self):
+        print(self.heap)
+
+    def insert(self, value):
+        self.heap.append(value)
+        current = len(self.heap) - 1
+        while current > 0 and self.heap[current] > self.heap[self._parent(current)]:
+            self._swap(current, self._parent(current))
+            current = self._parent(current)
+
+    def _sink_down(self, index):
+        max_index = index
+        while True:
+            left_index = self._left_child(index)
+            right_index = self._right_child(index)
+
+            if (left_index < len(self.heap) and
+                self.heap[left_index] > self.heap[max_index]):
+                max_index = left_index
+
+            if (right_index < len(self.heap) and
+                self.heap[right_index] > self.heap[max_index]):
+                max_index = right_index
+
+            if max_index != index:
+                self._swap(index, max_index)
+                index = max_index
+            else:
+                return
+
+    def remove(self):
+        if len(self.heap) == 0:
+            return None
+        if len(self.heap) == 1:
+            return self.heap.pop()
+
+        max_value = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._sink_down(0)
+
+        return max_value
+
+
+
+def _main():
+    my_heap = MaxHeap()
+
+    print("\n---------- insert method ----------")
+    my_heap.insert(99)
+    my_heap.insert(72)
+    my_heap.insert(61)
+    my_heap.insert(58)
+    my_heap.print_heap()
+    # print(my_heap.heap)
+
+    my_heap.insert(100)
+    my_heap.print_heap()
+
+    my_heap.insert(75)
+    my_heap.print_heap()
+
+    print("\n---------- remove method ----------")
+    max_val = my_heap.remove()
+    print(max_val)
+    my_heap.print_heap()
+
+
+def _exercises():
+    pass
+
+
+if __name__ == "__main__":
+    _main()
+    _exercises()
--- Data Structures/08_min_heaps.py
+++ Data Structures/08_min_heaps.py
@@ -1,8 +1,8 @@
 """
-Max Heaps
+Min Heaps
 """
 
-class MaxHeap:
+class MinHeap:
     def __init__(self):
         self.heap = []
 
@@ -24,28 +24,27 @@ class MaxHeap:
     def insert(self, value):
         self.heap.append(value)
         current = len(self.heap) - 1
-        while current > 0 and self.heap[current] > self.heap[self._parent(current)]:
+        while current > 0 and self.heap[current] < self.heap[self._parent(current)]:
             self._swap(current, self._parent(current))
             current = self._parent(current)
-        # return True
 
     def _sink_down(self, index):
-        max_index = index
+        min_index = index
         while True:
             left_index = self._left_child(index)
             right_index = self._right_child(index)
 
             if (left_index < len(self.heap) and
-                self.heap[left_index] > self.heap[max_index]):
-                max_index = left_index
+                self.heap[left_index] < self.heap[min_index]):
+                min_index = left_index
 
             if (right_index < len(self.heap) and
-                self.heap[right_index] > self.heap[max_index]):
-                max_index = right_index
+                self.heap[right_index] < self.heap[min_index]):
+                min_index = right_index
 
-            if max_index != index:
-                self._swap(index, max_index)
-                index = max_index
+            if min_index != index:
+                self._swap(index, min_index)
+                index = min_index
             else:
                 return
 
@@ -55,11 +54,11 @@ class MaxHeap:
         if len(self.heap) == 1:
             return self.heap.pop()
 
-        max_value = self.heap[0]
+        min_value = self.heap[0]
         self.heap[0] = self.heap.pop()
         self._sink_down(0)
 
-        return max_value
+        return min_value
 
 
 
