cfd96012791a8ee6d865c5ff42d09cbe96de7ccc
anshulpodaar <anshulpodaar@gmail.com>
2023-11-23 23:48:44 +0100

    Udemy DSA: 12 - Quick Sort

--- Data Structures/12_quick_sort.py
+++ Data Structures/12_quick_sort.py
@@ -6,70 +6,77 @@ import typing
 from typing import List
 
 
-def merge_sorted_lists(list1: List, list2: List) -> List:
-    combined = []
-    i = 0
-    j = 0
-    while i < len(list1) and j < len(list2):
-        if list1[i] < list2[j]:
-            combined.append(list1[i])
-            i += 1
-        else:
-            combined.append(list2[j])
-            j += 1
-    while i < len(list1):
-        combined.append(list1[i])
-        i += 1
-    while j < len(list2):
-        combined.append(list2[j])
-        j += 1
-    return combined
+def swap(my_list: List, index1: int, index2: int) -> None:
+    temp = my_list[index1]
+    my_list[index1] = my_list[index2]
+    my_list[index2] = temp
+    # return None
+
+
+def pivot(my_list: List, pivot_index: int, end_index: int) -> int:
+    swap_index = pivot_index
+    for i in range(pivot_index+1, end_index+1):
+        if my_list[i] < my_list[pivot_index]:
+            swap_index += 1
+            swap(my_list, swap_index, i)
+    swap(my_list, pivot_index, swap_index)
+    return swap_index
+
+
+def quick_sort_helper(my_list: List, left: int, right: int) -> List:
+    if left < right:
+        pivot_index = pivot(my_list, left, right)
+        quick_sort_helper(my_list, left, pivot_index-1)
+        quick_sort_helper(my_list, pivot_index+1, right)
+    return my_list
 
 
 def quick_sort(my_list: List) -> List:
-    pass
+    return quick_sort_helper(my_list, 0, len(my_list)-1)
 
 
 def _quick_sort():
     #########################
-    # Merge Helper Function
+    # Pivot Helper Function
     print("\n=========================")
-    print("Merge Helper Function")
+    print("PIVOT HELPER FUNCTION")
     # print("-------------------------")
 
-    print(merge_sorted_lists([1, 2, 7, 8], [3, 4, 5, 6]))
-    print(merge_sorted_lists([1, 2, 5, 6], [3, 4, 7, 8]))
-    print("=========================")
+    my_list = [4, 6, 1, 7, 3, 2, 5]
+    print("original List: ", my_list)
+    pivot_index = pivot(my_list, 0, 6)
+    print("Pivot index: ", pivot_index)
+    print("Pivoted List: ", my_list)
 
     #########################
     #########################
-    # Merge Sort
+    # Quick Sort
     print("\n=========================")
-    print("MERGE SORT")
+    print("QUICK SORT")
     # print("-------------------------")
 
-    original_list = [1, 2, 7, 8, 3, 4, 5, 6]
-    sorted_list = quick_sort(original_list)
-    print("original List: ", original_list)
-    print("Sorted list: ", sorted_list)
+    my_list = [1, 2, 7, 8, 3, 4, 5, 6]
+    print("original List: ", my_list)
+    my_list = quick_sort(my_list)
+    print("Sorted List: ", my_list)
 
     print()
 
-    original_list = [4, 6, 1, 7, 3, 2, 5]
-    sorted_list = quick_sort(original_list)
-    print("original List: ", original_list)
-    print("Sorted list: ", sorted_list)
+    my_list = [4, 6, 1, 7, 3, 2, 5]
+    print("original List: ", my_list)
+    my_list = quick_sort(my_list)
+    print("Sorted List: ", my_list)
 
     print()
 
+    my_list = [3, 2, 1]
+    print("original List: ", my_list)
+    my_list = quick_sort(my_list)
+    print("Sorted List: ", my_list)
 
-original_list = [3, 2, 1]
-    sorted_list = quick_sort(original_list)
-    print("original List: ", original_list)
-    print("Sorted list: ", sorted_list)
     print("=========================")
     #########################
 
 
 if __name__ == "__main__":
-    _merge_sort()
+    _quick_sort()
